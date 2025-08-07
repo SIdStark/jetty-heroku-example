d183e0f7f189432988f3809eecd8ca3ef483c453
Sid <siddharth.r.ravichandran@apisero.com>
2025-08-07 13:35:20 +0530

    Adding procfile

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -14,21 +14,21 @@ import org.eclipse.jetty.servlet.ServletHolder;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        // Use the port assigned by Heroku or default to 8080
+      
         int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
 
-        // Create a new Jetty server instance
+  
         Server server = new Server(port);
 
-        // Set up the context handler for servlets
+   
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
 
-        // Add our HelloWorldServlet to the context, mapping it to the "/hello" path
+      
         context.addServlet(new ServletHolder(new HelloWorldServlet()), "/hello");
 
-        // Start the server and join the thread to keep it running
+  
         server.start();
         server.join();
     }
